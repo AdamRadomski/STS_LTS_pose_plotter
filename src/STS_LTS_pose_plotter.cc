df1deb0f6b6bb7adee5ee56036d4e0ab74b9e41f
Adam Radomski <radam@student.ethz.ch>
2017-08-01 12:18:09 +0200

    Callbacks as class members. Added counters.

--- src/STS_LTS_pose_plotter.cc
+++ src/STS_LTS_pose_plotter.cc
@@ -1,38 +1,39 @@
 #include "STS_LTS_pose_plotter.h"
 
-#include <geometry_msgs/PoseStamped.h>
-
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+PosePlotter::PosePlotter() {
+  sts_msg_count_ = 0u;
+  lts_msg_count_ = 0u;
 }
 
-void poseCallbackSTS(const geometry_msgs::PoseStamped& msg)
+void PosePlotter::poseCallbackSTS(const geometry_msgs::PoseStamped& msg)
 {
-  ROS_INFO("STS heard a pose.");
+  ++sts_msg_count_;
+
+  ROS_INFO("STS heard a pose. [%d]", sts_msg_count_);
 
 }
 
-void poseCallbackLTS(const geometry_msgs::PoseStamped& msg)
+void PosePlotter::poseCallbackLTS(const geometry_msgs::PoseStamped& msg)
 {
-  ROS_INFO("LTS heard a pose.");
+  ++lts_msg_count_;
+
+  ROS_INFO("LTS heard a pose. [%d]", lts_msg_count_);
 
 }
 
 int main(int argc, char **argv)
 {
   std::cout << "*** pose plotter starts running ***" << std::endl;
-
-
   ros::init(argc, argv, "pose_plotter");
 
   // Subscribe to nodes.
+  PosePlotter plotter;
   ros::NodeHandle n;
-  ros::Subscriber sub_sts = n.subscribe("/fw_pose/sts", 1000, poseCallbackSTS);
-  ros::Subscriber sub_lts = n.subscribe("/fw_pose/lts", 1000, poseCallbackLTS);
+  ros::Subscriber sub_sts = n.subscribe("/fw_pose/sts", 1000, &PosePlotter::poseCallbackSTS, &plotter);
+  ros::Subscriber sub_lts = n.subscribe("/fw_pose/lts", 1000, &PosePlotter::poseCallbackLTS, &plotter);
 
   ros::spin();
 
--- src/STS_LTS_pose_plotter.h
+++ src/STS_LTS_pose_plotter.h
@@ -1,4 +1,21 @@
 #pragma once
 
+#include <geometry_msgs/PoseStamped.h>
+
+class PosePlotter {
+ public:
+  PosePlotter();
+
+  // Callbacks called when messages are received.
+  void poseCallbackSTS(const geometry_msgs::PoseStamped& msg);
+  void poseCallbackLTS(const geometry_msgs::PoseStamped& msg);
+
+ private:
+  // Counters
+  size_t sts_msg_count_;
+  size_t lts_msg_count_;
+
+};
+
 // Nothing here yet
 
